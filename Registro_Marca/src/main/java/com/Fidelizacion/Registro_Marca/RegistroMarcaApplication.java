099883a8392b45424c05455dd1b238eb9d082a7e
JuanZB360 <sjzaba2023@gamil.com>
2026-09-04 16:38:41 -0500

    Inicializacion del proyecto de fidelizacion de clientes a las marcas de la empresa

---
+++ Registro_Marca/src/main/java/com/Fidelizacion/Registro_Marca/RegistroMarcaApplication.java
@@ -0,0 +1,13 @@
+package com.Fidelizacion.Registro_Marca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroMarcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroMarcaApplication.class, args);
+	}
+
+}
---
+++ Registro_Marca/src/test/java/com/Fidelizacion/Registro_Marca/RegistroMarcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fidelizacion.Registro_Marca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroMarcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
