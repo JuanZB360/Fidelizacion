a2c2eccfb2615cf7e1ff09cc113b2c02987aa058
JuanZB360 <sjzaba2023@gamil.com>
2026-09-05 00:35:21 -0500

    creacion de las relacion entre las tablas usuarios marcas y ubicacion

--- Registro_Marca/src/main/java/com/Fidelizacion/Registro_Marca/modelos/Usuario.java
+++ Registro_Marca/src/main/java/com/Fidelizacion/Registro_Marca/modelos/Usuario.java
@@ -9,9 +9,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -58,5 +61,12 @@ public class Usuario {
 
     @Column(name = "fecha_nacimiento", nullable = false)
     private LocalDate fechaNacimiento;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Ubicacion direccion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn (name = "marca_id")
+    private Marca marca;
 
 }
