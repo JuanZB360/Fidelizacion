48118f031ad7b66e99857bf83ac90fd278bfeb36
Juan David Zapata Barrera <163927311+JuanZB360@users.noreply.github.com>
2026-09-07 17:22:36 -0500

    Revert "correccion de errores resultantes de validar conexion con front end"

--- registromarca/src/services/tipoDocumentoService.js
+++
@@ -1,6 +0,0 @@
-import api from './apiBack';
-
-export const tipoDocumentoService = {
-  listar: () => api.get('/tipo-documento'),
-  buscarPorId: (id) => api.get(`/tipo-documento/${id}`),
-};
--- registromarca/src/services/usuarioService.js
+++
@@ -1,8 +0,0 @@
-import api from './apiBack';
-
-export const usuarioService = {
-  crear: (datos) => api.post('/usuario', datos),
-  listar: () => api.get('/usuario'),
-  buscarPorId: (id) => api.get(`/usuario/${id}`),
-  actualizar: (id, datos) => api.patch(`/usuario/${id}`, datos),
-};
