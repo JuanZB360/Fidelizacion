f76a69f923075b2015d388c2b48de588c568e4ac
JuanZB360 <sjzaba2023@gamil.com>
2026-09-07 21:32:14 -0500

    creacion del archivo para estructurar la base de datos e insertar informacion en ella

--- Registro_Marca/src/main/java/com/Fidelizacion/Registro_Marca/RegistroMarcaApplication.java
+++ Registro_Marca/src/main/java/com/Fidelizacion/Registro_Marca/RegistroMarcaApplication.java
@@ -1,15 +1,19 @@
 package com.Fidelizacion.Registro_Marca;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync 
 public class RegistroMarcaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistroMarcaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Carga el archivo .env si existe en la raíz
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 
+        SpringApplication.run(RegistroMarcaApplication.class, args);
+    }
 }
